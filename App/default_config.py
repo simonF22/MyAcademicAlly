c2eb9cdd5b22c338793a101322543a5a5280f0bf
simonF22 <marc.ramatali@outlook.com>
2023-10-25 16:54:53 +0000

    commit

--- App/default_config.py
+++ App/default_config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "postgres://myacademically_user:n7h029R3TgevotwbR6xNwvoDrqMOToaa@dpg-cksk8r9rfc9c7397jd3g-a/myacademically"
+SQLALCHEMY_DATABASE_URI = "postgresql:///temp-database.db"
 SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 7
-ENV = "PRODUCTION"
+ENV = "DEVELOPMENT"
